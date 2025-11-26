804ead39e886cb5d5bd984fca594f523f68705b5
Sushant Neupane <nsushaant72@gmail.com>
2025-11-26 21:59:18 +0545

    initialized weather app project

---
+++ lib/app_colors.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  // Primary Colors
+  static const Color primaryPurple = Color(0xFF7C6FDC);
+  static const Color darkPurple = Color(0xFF2E3350);
+  static const Color lightPurple = Color(0xFF9B8FE8);
+
+  // Accent Color
+  static const Color accentYellow = Color(0xFFFDB742);
+
+  // Text Colors
+  static const Color textWhite = Color(0xFFFFFFFF);
+  static const Color textGray = Color(0xFFB0B0B0);
+}
